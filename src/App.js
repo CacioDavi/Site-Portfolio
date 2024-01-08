cf0f8560c30e1b8622731adf07132975e9cf9d95
caciodavi <davicacio4@gmail.com>
2024-01-07 22:15:14 -0300

    ajuste para o github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./Components/Components/Pages/Home";
 import Portifolio from "./Components/Components/Pages/Portifolio";
 import Contatos from "./Components/Components/Pages/Contatos";
 import Sobre from "./Components/Components/Pages/Sobre";
 
 function App() {
-
+  // alert(process.env.PUBLIC_URL)
 
 
   return (
     <div className="app">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />}  />
-          <Route path="port" element={<Portifolio />} />
-          <Route path="about" element={<Sobre />} />
-          <Route path="contact" element={<Contatos />} />
+          <Route path="/port" element={<Portifolio />} />
+          <Route path="/about" element={<Sobre />} />
+          <Route path="/contact" element={<Contatos />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
 
 
     </div>
